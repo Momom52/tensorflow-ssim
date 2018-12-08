2c42533435feea40d68abc02f30f94952090fc88
Aldy <aldysdu@gmail.com>
2018-12-08 18:16:54 +0800

    update the ssim.py

--- SSIM.py
+++ SSIM.py
@@ -15,14 +15,15 @@ def img_resize(img1, img2, width=641, height=641):
     return new_img1, new_img2
 
 def gaussian(window_size, sigma = 1.5):
-    window = [np.exp(-(x - window_size//2)**2 / float(2*sigma**2)) for x in range(window_size)]
-    window = [window, window]
-    #print(window)
-    window = tf.convert_to_tensor(window)
+    """
+    the function is to calculate te Gaussian weighting
+    """
+    y, x = np.ogrid[-window_size//2 : window_size//2, -window_size//2 : window_size//2]
+    window = np.exp(-(x**2 + y**2) / np.float(2*sigma**2))
+    window = tf.convert_to_tensor(np.float32(window))
     window /= tf.reduce_sum(window, 0)
     window = tf.expand_dims(window, -1)
     window = tf.expand_dims(window, -1)
-    #print(window)
 
     return window
 
@@ -30,7 +31,6 @@ def ssim(img1, img2, k1=0.01, k2=0.02, L=255, window_size=11):
     """
     The function is to calculate the ssim score
     Img1's shape should be equal to img2's shape
-
     """
     if not img1.shape == img2.shape:
         img1, img2 = img_resize(img1, img2)
@@ -40,7 +40,7 @@ def ssim(img1, img2, k1=0.01, k2=0.02, L=255, window_size=11):
     if img2.dtype == np.uint8:
         img2 = np.float32(img2)
 
-    h,w = img1.shape
+    #h,w = img1.shape
 
     img1 = tf.expand_dims(img1, 0)
     img1 = tf.expand_dims(img1, -1)
@@ -49,16 +49,16 @@ def ssim(img1, img2, k1=0.01, k2=0.02, L=255, window_size=11):
 
     window = gaussian(window_size)
 
-    mu1 = tf.nn.conv2d(img1, window, strides = [1, h, w, 1], padding = 'VALID')
-    mu2 = tf.nn.conv2d(img2, window, strides = [1, h, w, 1], padding = 'VALID')
+    mu1 = tf.nn.conv2d(img1, window, strides = [1, 1, 1, 1], padding = 'VALID')
+    mu2 = tf.nn.conv2d(img2, window, strides = [1, 1, 1, 1], padding = 'VALID')
 
     mu1_sq = mu1 * mu1
     mu2_sq = mu2 * mu2
     mu1_mu2 = mu1 * mu2
 
-    sigma1_sq = tf.nn.conv2d(img1*img1, window, strides = [1 ,h, w, 1], padding = 'VALID') - mu1_sq
-    sigma2_sq = tf.nn.conv2d(img2*img2, window, strides = [1, h, w, 1], padding = 'VALID') - mu2_sq
-    sigma1_2 = tf.nn.conv2d(img1*img2, window, strides = [1, h, w, 1], padding = 'VALID') - mu1_mu2
+    sigma1_sq = tf.nn.conv2d(img1*img1, window, strides = [1 ,1, 1, 1], padding = 'VALID') - mu1_sq
+    sigma2_sq = tf.nn.conv2d(img2*img2, window, strides = [1, 1, 1, 1], padding = 'VALID') - mu2_sq
+    sigma1_2 = tf.nn.conv2d(img1*img2, window, strides = [1, 1, 1, 1], padding = 'VALID') - mu1_mu2
 
     c1 = (k1*L)**2
     c2 = (k2*L)**2
@@ -76,8 +76,8 @@ def ssim(img1, img2, k1=0.01, k2=0.02, L=255, window_size=11):
 
 
 if __name__ == '__main__':
-    img1 = np.asarray(cv2.imread(path, 0))
-    img2 = np.asarray(cv2.imread(path, 0))
+    img1 = np.asarray(cv2.imread('/home/aldy/桌面/result.3.png', 0))
+    img2 = np.asarray(cv2.imread('/home/aldy/桌面/000001_0.3.jpg', 0))
     score = ssim(img1,img2)
 
     print(score)
